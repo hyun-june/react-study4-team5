c56ed2861b325b521b7d8ed34175cacb32279df9
unknown <leehyunjune11@naver.com>
2025-11-18 16:33:25 +0900

    feat: add weather api

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,17 @@
+import { getWeatherApi, getWeatherByCity } from "../utils/apis/weatherAPI";
+
 const Login = () => {
+  // const getWeather = async () => {
+  //   const data = await getWeatherApi();
+  // };
+
+  // getWeather();
+  const getWeatherCity = async () => {
+    const data = await getWeatherByCity();
+    console.log(data);
+  };
+  getWeatherCity();
+
   return <div>Login</div>;
 };
 
