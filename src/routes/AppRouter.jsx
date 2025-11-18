83c42fe720521848882a4cd30f38fe89bf19d772
JiHyoung You <fbwlgud51@gmail.com>
2025-11-18 21:22:42 +0900

    chore: restore root path

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -3,12 +3,11 @@ import Home from "../pages/Home";
 import Cities from "./../pages/Cities";
 import CityDetail from "./../pages/CityDetail";
 import Login from "./../pages/Login";
-import Exchange from "../pages/Exchange";
 
 const AppRouter = () => {
   return (
     <Routes>
-      <Route path="/" element={<Exchange />} />
+      <Route path="/" element={<Home />} />
       <Route path="/cities" element={<Cities />} />
       <Route path="/city/detail" element={<CityDetail />} />
       <Route path="/login" element={<Login />} />
