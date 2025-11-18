4c4452cb698fab1e4a5e2d46aa7cacd25ed43718
unknown <leehyunjune11@naver.com>
2025-11-18 17:26:10 +0900

    feat: add Login & zustand Login store

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,33 @@
-import { getWeatherApi, getWeatherByCity } from "../utils/apis/weatherAPI";
+import styled from "@emotion/styled";
+import { getWeatherByCity } from "../utils/apis/weatherAPI";
+import { Box, Button, FormGroup, TextField } from "@mui/material";
+import { useLoginStore } from "../store/useLoginStore";
+import { useState } from "react";
+// import { getWeatherApi } from "../utils/apis/weatherAPI";
+
+const LoginInputGroup = styled(FormGroup)({
+  gap: 15,
+});
+
+const LoginInput = styled(TextField)({
+  "& .MuiInputBase-input": {
+    fontSize: 16,
+  },
+
+  "& .MuiInputLabel-root": {
+    fontSize: 14,
+  },
+});
+
+const LoginButton = styled(Button)({
+  border: "1px solid black",
+});
 
 const Login = () => {
+  const { login } = useLoginStore();
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+
   // const getWeather = async () => {
   //   const data = await getWeatherApi();
   // };
@@ -8,11 +35,39 @@ const Login = () => {
   // getWeather();
   const getWeatherCity = async () => {
     const data = await getWeatherByCity();
-    console.log(data);
+    // console.log(data);
   };
   getWeatherCity();
 
-  return <div>Login</div>;
+  const handleLogin = () => {
+    login(id, password);
+  };
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+      }}
+    >
+      <LoginInputGroup>
+        <LoginInput
+          label="ID"
+          size="small"
+          onChange={(e) => setId(e.target.value)}
+        />
+        <LoginInput
+          label="PASSWORD"
+          size="small"
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <LoginButton onClick={handleLogin}>Login</LoginButton>
+      </LoginInputGroup>
+    </Box>
+  );
 };
 
 export default Login;
