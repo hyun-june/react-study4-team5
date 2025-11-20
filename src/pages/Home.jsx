4d9dce597a8f5516a40518e492ed3e144e291776
JiHyoung You <fbwlgud51@gmail.com>
2025-11-20 18:56:15 +0900

    style: improve currency converter design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,33 @@ import CurrencyConverter from "../components/CurrencyConverter";
 
 const Home = () => {
   return (
-    <Box>
+    <Box sx={{ position: "relative" }}>
       <Box
-        component="img"
-        src="https://images.pexels.com/photos/460672/pexels-photo-460672.jpeg"
-        width={"100%"}
-      />
+        sx={{
+          position: "relative",
+          width: "100%",
+          overflow: "hidden",
+        }}
+      >
+        <Box
+          component="img"
+          src="https://images.pexels.com/photos/460672/pexels-photo-460672.jpeg"
+          sx={{
+            width: "100%",
+            display: "block",
+          }}
+        />
+        <Box
+          sx={{
+            position: "absolute",
+            bottom: 0,
+            left: 0,
+            right: 0,
+            height: "200px",
+            background: "linear-gradient(to bottom, transparent, #ffffff)",
+          }}
+        />
+      </Box>
       <CurrencyConverter />
     </Box>
   );
